7b171064f44f21ce3a2c056676a3d7d190707b41
Manish N Devadiga <117370687+Manishndevadiga@users.noreply.github.com>
2023-04-22 01:50:37 -0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -26,3 +26,41 @@ if (mysqli_query($conn, $sql))
 mysqli_close($conn);
 
 ?>
+
+//creating a table
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = "myDB1";
+$conn = mysqli_connect($servername, $username, $password, $db);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+
+echo "<br>";
+
+// Creating a table in mysql using php
+
+    $sql = "CREATE TABLE MyGuests (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(30) NOT NULL,
+    lastname VARCHAR(30) NOT NULL,
+    email VARCHAR(50)
+    )";
+
+if (mysqli_query($conn, $sql)) {
+    echo "Table MyGuests created successfully";
+  } else {
+    echo "Error creating table: " . mysqli_error($conn);
+  }
+
+mysqli_close($conn);
+
+?>
+
+
